05e1b77c4efbfc71b1f6541d5c155aeadf23caeb
sonylomo <sonylomo1@gmail.com>
2022-03-09 20:07:44 +0300

    Deployed successfully 🚀

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,6 +10,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
+const ALCHEMY_API_KEY = "lG94bu_LGHBK9TiXFjdkgCSjbFesav9c";
+const ROPSTEN_PRIVATE_KEY = "cc4759ec693fddc7f5e1a9e0ee1a10aabdb5562de885064b6405651592a09efe";
+
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
@@ -18,4 +22,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.0",
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`${ROPSTEN_PRIVATE_KEY}`]
+    }
+  }
 };
